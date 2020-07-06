b76d9875e6ac816474d9b74a582c7e82567b614e
Steve Booth <scriptmcuk@gmail.com>
2020-07-06 11:57:37 +0100

    Add PUT route to Users

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 from project.api.models import User
 
 
@@ -111,3 +113,46 @@ def test_remove_user_incorrect_id(test_app, test_database, add_user):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 404
     assert "User 111 does not exist" in data["message"]
+
+
+def test_update_user(test_app, test_database, add_user):
+    user = add_user("user-to-be-updated", "update-me@gmail.com")
+    client = test_app.test_client()
+    resp_one = client.put(
+        f"/users/{user.id}",
+        data=json.dumps({"username": "me", "email": "me@gmail.com"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert f"{user.id} was updated!" in data["message"]
+    resp_two = client.get(f"/users/{user.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "me" in data["username"]
+    assert "me@gmail.com" in data["email"]
+
+
+@pytest.mark.parametrize(
+    "user_id, payload, status_code, message",
+    [
+        [1, {}, 400, "Input payload validation failed"],
+        [1, {"email": "me@gmail.com"}, 400, "Input payload validation failed"],
+        [
+            999,
+            {"username": "me", "email": "me@gmail.com"},
+            404,
+            "User 999 does not exist",
+        ],
+    ],
+)
+def test_update_user_invalid(
+    test_app, test_database, user_id, payload, status_code, message
+):
+    client = test_app.test_client()
+    resp = client.put(
+        f"/users/{user_id}", data=json.dumps(payload), content_type="application/json"
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == status_code
+    assert message in data["message"]
